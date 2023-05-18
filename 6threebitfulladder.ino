e374ff0ce88a878895c04c0b2ff281d221463bcd
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2023-05-18 08:28:53 -0400

    Update 6threebitfulladder.ino

--- 6threebitfulladder.ino
+++ 6threebitfulladder.ino
@@ -2,40 +2,38 @@
   Created by Cristiano S
   Created in May 2023
 
-  Gives inputs to a 3-bit full adder
+  Gives inputs to a 2-bit full adder and subtractor in two's compliment
 */
 
-unsigned int inputAOnePin = 13;
-unsigned int inputBOnePin = 12;
-unsigned int inputATwoPin = 11;
-unsigned int inputBTwoPin = 10;
-unsigned int inputAThreePin = 9;
-unsigned int inputBThreePin = 8;
+unsigned int inputSignPin = 12;
+unsigned int inputAOnePin = 11;
+unsigned int inputBOnePin = 10;
+unsigned int inputATwoPin = 9;
+unsigned int inputBTwoPin = 8;
 
 void setup()
 {
   // Pin inputs
+  pinMode(inputSignPin, OUTPUT);
   pinMode(inputAOnePin, OUTPUT);
   pinMode(inputBOnePin, OUTPUT);
   pinMode(inputATwoPin, OUTPUT);
   pinMode(inputBTwoPin, OUTPUT);
-  pinMode(inputAThreePin, OUTPUT);
-  pinMode(inputBThreePin, OUTPUT);
   // Serial monitor
-  Serial.begin(9600); // Start the serial monitor at 9600 baud
-  Serial.println("\nStart:  ...\n");
+  Serial.begin(9600);                // Start the serial monitor at 9600 baud
+  Serial.println("\nStart:  ...\n"); // Print to serial monitor on a new line
 }
 
 void loop()
 {
-  for (int inputAThree = 0; inputAThree < 2; inputAThree++) {
-    // Set the a3 pin to on or off if inputAThree is 1 or another number
-    switch (inputAThree) {
+  for (int inputSign = 0; inputSign < 2; inputSign++) {
+    // Set the sign pin to on or off if inputATwo is 1 or another number
+    switch (inputSign) {
       case 1:
-        digitalWrite(inputAThreePin, HIGH);
+        digitalWrite(inputSignPin, HIGH);
         break;
       default:
-        digitalWrite(inputAThreePin, LOW);
+        digitalWrite(inputSignPin, LOW);
         break;
     }
     for (int inputATwo = 0; inputATwo < 2; inputATwo++) {
@@ -58,74 +56,63 @@ void loop()
             digitalWrite(inputAOnePin, LOW);
             break;
         }
-        for (int inputBThree = 0; inputBThree < 2; inputBThree++) {
-          // Set the b3 pin to on or off if inputBThree is 1 or another number
-          switch (inputBThree) {
+        for (int inputBTwo = 0; inputBTwo < 2; inputBTwo++) {
+          // Set the b2 pin to on or off if inputBTwo is 1 or another number
+          switch (inputBTwo) {
             case 1:
-              digitalWrite(inputBThreePin, HIGH);
+              digitalWrite(inputBTwoPin, HIGH);
               break;
             default:
-              digitalWrite(inputBThreePin, LOW);
+              digitalWrite(inputBTwoPin, LOW);
               break;
           }
-          for (int inputBTwo = 0; inputBTwo < 2; inputBTwo++) {
-            // Set the b2 pin to on or off if inputBTwo is 1 or another number
-            switch (inputBTwo) {
+          for (int inputBOne = 0; inputBOne < 2; inputBOne++) {
+            // Set the b1 pin to on or off if inputBOne is 1 or another number
+            switch (inputBOne) {
               case 1:
-                digitalWrite(inputBTwoPin, HIGH);
+                digitalWrite(inputBOnePin, HIGH);
                 break;
               default:
-                digitalWrite(inputBTwoPin, LOW);
+                digitalWrite(inputBOnePin, LOW);
                 break;
             }
-            for (int inputBOne = 0; inputBOne < 2; inputBOne++) {
-              // Set the b1 pin to on or off if inputBOne is 1 or another number
-              switch (inputBOne) {
-                case 1:
-                  digitalWrite(inputBOnePin, HIGH);
-                  break;
-                default:
-                  digitalWrite(inputBOnePin, LOW);
-                  break;
-              }
-              // 3-Bit Adder math
-              int xorGateOne = inputAOne ^ inputBOne;
-              int xorGateOneTwo = 0 ^ xorGateOne;
-              int andGateOne = inputAOne & inputBOne;
-              int andGateOneTwo = 0 & xorGateOne;
-              int orGateOne = andGateOne | andGateOneTwo;
-              int carryOutOne = orGateOne;
-              int xorGateTwoOne = inputATwo ^ inputBTwo;
-              int xorGateTwoTwo = carryOutOne ^ xorGateTwoOne;
-              int andGateTwoOne = inputATwo & inputBTwo;
-              int andGateTwoTwo = carryOutOne & xorGateTwoOne;
-              int orGateTwo = andGateTwoOne | andGateTwoTwo;
-              int carryOutTwo = orGateTwo;
-              int xorGateThreeOne = inputAThree ^ inputBThree;
-              int xorGateThreeTwo = carryOutTwo ^ xorGateThreeOne;
-              int andGateThreeOne = inputAThree & inputBThree;
-              int andGateThreeTwo = carryOutTwo & xorGateThreeOne;
-              int orGateThree = andGateThreeOne | andGateThreeTwo;
-              int carryOutThree = orGateThree;
-              int sumOne = xorGateOneTwo;
-              int sumTwo = xorGateTwoTwo;
-              int sumThree = xorGateThreeTwo;
+            // 2-Bit adder with subtraction logic
+            int xorGateSp = inputSign ^ inputBOne;
+            int xorGateSp2 = inputSign ^ inputBTwo;
+            int xorGateOne = inputAOne ^ xorGateSp;
+            int xorGateOne2 = xorGateOne ^ inputSign;
+            int andGateOne = inputAOne & xorGateSp;
+            int andGateOne2 = inputSign & xorGateOne;
+            int orGateOne = andGateOne | andGateOne2;
+            int carryOutOne = orGateOne;
+            int xorGateTwo = inputATwo ^ xorGateSp2;
+            int xorGateTwo2 = xorGateTwo ^ carryOutOne;
+            int andGateTwo = inputATwo & xorGateSp2;
+            int andGateTwo2 = xorGateTwo & carryOutOne;
+            int orGateTwo = andGateTwo | andGateTwo2;
+            int carryOutTwo = orGateTwo;
+            int sumOne = xorGateOne2;
+            int sumTwo = xorGateTwo2;
 
-              Serial.print(inputAThree);
-              Serial.print(inputATwo);
-              Serial.print(inputAOne);
-              Serial.print(" + ");
-              Serial.print(inputBThree);
-              Serial.print(inputBTwo);
-              Serial.print(inputBOne);
-              Serial.print(" = ");
-              Serial.print(sumThree);
-              Serial.print(sumTwo);
-              Serial.print(sumOne);
-              Serial.print(" Cout ");
-              Serial.println(carryOutThree);
-              delay(2500);
+            // Print calcuation to serial monitor
+            char signText = '+';
+            if (inputSign == 1) {
+              signText = '-';
             }
+
+            Serial.print(inputATwo);
+            Serial.print(inputAOne);
+            Serial.print(" ");
+            Serial.print(signText);
+            Serial.print(" ");
+            Serial.print(inputBTwo);
+            Serial.print(inputBOne);
+            Serial.print(" = ");
+            Serial.print(sumTwo);
+            Serial.print(sumOne);
+            Serial.print(" Cout ");
+            Serial.println(carryOutTwo);
+            delay(3000);
           }
         }
       }
